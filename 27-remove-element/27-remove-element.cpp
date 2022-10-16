ee1bae88ca3ad47a1e4559c5a1a1408a887222ba
Harsh Srivastava <82330033+Harshstar@users.noreply.github.com>
2022-10-16 22:25:17 +0530

    Update 27-remove-element.cpp

--- 27-remove-element/27-remove-element.cpp
+++ 27-remove-element/27-remove-element.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         int i,r=0;
-        for(i=0;i<nums.size();i++)
+        for(i=0;i<=nums.size()-1;i++)
         {
             if(nums[i]!=val)
                 nums[r++]=nums[i];
