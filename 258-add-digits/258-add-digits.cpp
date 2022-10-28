c064c148a43804bde793b1775eeac2dcb999fff5
Harsh Srivastava <82330033+Harshstar@users.noreply.github.com>
2022-10-28 21:41:40 +0530

    Update 258-add-digits.cpp

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int addDigits(int num) {
         
-        while(num > 9 )
+        while(num >9)
         {
             int temp=num,sum=0;
             while(temp>0)
