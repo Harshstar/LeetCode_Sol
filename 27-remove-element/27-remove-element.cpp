05f17cb3519ecaa7b7f731294a61f69b49e7620b
Harsh Srivastava <82330033+Harshstar@users.noreply.github.com>
2022-10-30 21:46:02 +0530

    Update 27-remove-element.cpp

--- 27-remove-element/27-remove-element.cpp
+++ 27-remove-element/27-remove-element.cpp
@@ -4,7 +4,7 @@ public:
         int i,r=0;
         for(i=0;i<=nums.size()-1;i++)
         {
-            if(nums[i]!=val)
+            if(nums[i] != val)
                 nums[r++]=nums[i];
         }
         return r;
