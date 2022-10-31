b8a88c28aba374012ddda1220f5ba75683dd8f80
Harsh Srivastava <82330033+Harshstar@users.noreply.github.com>
2022-10-31 20:37:58 +0530

    Time: 12 ms (37.25%), Space: 9.6 MB (72.03%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int flag=0,i;
+        for(i=0;i<nums.size();i++)
+        {
+            if(nums[i]==target)
+            {
+                flag=0;
+                break;
+            }
+            else if(nums[i]>target)
+            { 
+                flag=1; 
+                break;
+            }
+        }
+            return i;
+    }
+};
