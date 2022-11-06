80d03436514b1abd7ba2c5d04f07bf4bb6ff1f31
Harsh Srivastava <82330033+Harshstar@users.noreply.github.com>
2022-11-06 17:10:57 +0530

    Update 0746-min-cost-climbing-stairs.cpp

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -5,6 +5,6 @@ public:
         for(int i=2 ; i<n ; i++){
             cost[i] = min(cost[i-1] , cost[i-2]) + cost[i];
         }
-        return min(cost[n-1] , cost[n-2]);
+        return min(cost[n-1] ,cost[n-2]);
     }
 };
