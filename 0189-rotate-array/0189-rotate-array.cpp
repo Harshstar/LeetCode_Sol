bc53cf600783be6fda142807bb829a4fd4ee31bd
Harsh Srivastava <82330033+Harshstar@users.noreply.github.com>
2022-11-17 23:45:49 +0530

    Update 0189-rotate-array.cpp

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,7 +3,7 @@ public:
     void rotate(vector<int>& nums, int k) {
         vector <int> temp;
         k%=nums.size();
-        int n=nums.size()-k;
+        int  n=nums.size()-k;
         for(int i=n;i<nums.size();i++)
             temp.push_back(nums[i]);
         for(int i=0;i<n;i++)
