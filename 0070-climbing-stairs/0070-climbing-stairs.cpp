bbb4673818207601e24d54a150ff9f8f21abf706
Harsh Srivastava <82330033+Harshstar@users.noreply.github.com>
2022-11-20 23:04:40 +0530

    Update 0070-climbing-stairs.cpp

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -6,7 +6,7 @@ public:
         arr[1]=1;
         for(int i=2;i<=n;i++)
         {
-            arr[i]=arr[i-1]+arr[i-2];
+            arr[i]= arr[i-1]+arr[i-2];
         }
         return arr[n];
     }
