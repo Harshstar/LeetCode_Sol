df185b3afb749e60abfdd82b36e90bf44f2ce8a7
Harsh Srivastava <82330033+Harshstar@users.noreply.github.com>
2022-12-01 20:59:44 +0530

    Update 0118-pascals-triangle.cpp

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -7,7 +7,7 @@ public:
             arr[i].resize(i+1);
             for(int j=0;j<=i;j++)
             {
-                if(j==0or j==i)
+                if(j==0orj==i)
                      arr[i][j]=1;
                 else
                      arr[i][j]=arr[i-1][j-1]+arr[i-1][j];
