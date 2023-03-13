9bd3ba83081eff950974f9d41a2f7803915c21d6
Harsh Srivastava <82330033+Harshstar@users.noreply.github.com>
2023-03-13 22:33:10 +0530

    Time: 7 ms (53.98%), Space: 16.4 MB (84.12%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isEqual(TreeNode*r1,TreeNode*r2){
+        
+        if(!r1||!r2)
+            return r1==r2;
+      
+        if(r1->val==r2->val){
+
+            return isEqual(r1->left,r2->right)&&isEqual(r1->right,r2->left);
+        }
+  
+        return false;
+    }
+    bool isSymmetric(TreeNode* root) {
+    
+        return isEqual(root->left,root->right);
+    }
+};
