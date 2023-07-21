33785fad9ec6dc08755716826ff28ea3f0afaf11
Harsh Srivastava <82330033+Harshstar@users.noreply.github.com>
2023-07-21 22:25:24 +0530

    Time: 4 ms (27.18%), Space: 11.2 MB (5.33%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -1,17 +1,43 @@
 class Solution {
 public:
+
+    void inorder(TreeNode* root, vector<int>&v)
+    {
+        if(root!=NULL)
+        {
+            inorder(root->left,v);
+            v.push_back(root->val);
+            inorder(root->right,v);
+        }
+        else{
+            v.push_back(100000);
+        }
+    }
+    void preorder(TreeNode* root, vector<int>&v)
+    {
+        if(root!=NULL)
+        {
+            v.push_back(root->val);
+            preorder(root->left,v);
+            preorder(root->right,v);
+        }
+        else{
+            v.push_back(100000);
+        }
+    }
+
     bool isSameTree(TreeNode* p, TreeNode* q)
     {
-        stack<pair<TreeNode*, TreeNode*>> st;
-        st.push({p, q});
-        while (!st.empty()) {
-            auto [node1, node2] = st.top();
-            st.pop();
-            if (!node1 && !node2) continue;
-            if (!node1 || !node2 || node1->val != node2->val) return false;
-            st.push({node1->left, node2->left});
-            st.push({node1->right, node2->right});
-        }
-        return true;
+        vector<int> inp,inq,prep,preq;
+        inorder(p,inp);
+        inorder(q,inq);
+
+        preorder(p,prep);
+        preorder(q,preq);
+
+        if(inp==inq && prep==preq)
+            return true;
+        else
+            return false;
     }
 };
