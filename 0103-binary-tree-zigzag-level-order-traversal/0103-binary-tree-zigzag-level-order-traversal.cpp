c57242374285490694ed3e3ac153bcc7d9853e8d
Harsh Srivastava <82330033+Harshstar@users.noreply.github.com>
2023-07-26 22:55:03 +0530

    Time: 11 ms (5.48%), Space: 12.1 MB (23.38%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -1,28 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        int i=0;
         vector<vector<int>>ans;
-        if(!root)return ans;
         queue<TreeNode*>q;
         q.push(root);
-        int i=0;
-        while(!q.empty()){
-           int sz=q.size();
-           vector<int>v;
-           while(sz--){
-               TreeNode * f=q.front();
-               v.push_back(q.front()->val);
-               q.pop();
-               if(f->left)q.push(f->left);
-               if(f->right)q.push(f->right);
+        if(root==NULL)
+            return ans;
+        while(1)
+        {
+            int size=q.size();
+            if(size==0)
+                return ans;
 
-           }
-           if(i++%2)
-           reverse(v.begin(),v.end());
-           ans.push_back(v);
-
+            vector<int>data;
+            while(size>0)
+            {
+                TreeNode* temp=q.front();
+                q.pop();
+                data.push_back(temp->val);
+                if(temp->left!=NULL)
+                    q.push(temp->left);
+                if(temp->right!=NULL)
+                    q.push(temp->right);
+                size--;
+            }
+            if(i%2!=0)
+                reverse(data.begin(),data.end());
+            ans.push_back(data);
+            i++;
+            
         }
         return ans;
-    
     }
 };
