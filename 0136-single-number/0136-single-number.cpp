35acc4b098d2cfda74143df371b3dd334d7b3330
Harsh Srivastava <82330033+Harshstar@users.noreply.github.com>
2023-08-08 01:26:31 +0530

    Time: 31 ms (22.04%), Space: 20.7 MB (6.09%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        map<int,int>mp;
+        for(auto x:nums)
+            mp[x]++;
+        for(auto x:mp)
+        {
+            if(x.second!=2)
+                return x.first;
+        }
+        return -1;
+    }
+};
