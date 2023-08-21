4583d2bc6781392cd69468c9fe1d922fe31a2123
Harsh Srivastava <82330033+Harshstar@users.noreply.github.com>
2023-08-21 18:35:41 +0530

    Time: 0 ms (100%), Space: 10.2 MB (8.43%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -1,43 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-
-    void inorder(TreeNode* root, vector<int>&v)
-    {
-        if(root!=NULL)
-        {
-            inorder(root->left,v);
-            v.push_back(root->val);
-            inorder(root->right,v);
-        }
-        else{
-            v.push_back(100000);
-        }
-    }
-    void preorder(TreeNode* root, vector<int>&v)
-    {
-        if(root!=NULL)
-        {
-            v.push_back(root->val);
-            preorder(root->left,v);
-            preorder(root->right,v);
-        }
-        else{
-            v.push_back(100000);
-        }
-    }
-
-    bool isSameTree(TreeNode* p, TreeNode* q)
-    {
-        vector<int> inp,inq,prep,preq;
-        inorder(p,inp);
-        inorder(q,inq);
-
-        preorder(p,prep);
-        preorder(q,preq);
-
-        if(inp==inq && prep==preq)
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p==NULL && q==NULL)
             return true;
-        else
+        if(p==NULL || q==NULL)
             return false;
+        if(p->val != q->val)
+            return false;
+        
+        return isSameTree(p->left,q->left)&&isSameTree(p->right,q->right);
     }
 };
