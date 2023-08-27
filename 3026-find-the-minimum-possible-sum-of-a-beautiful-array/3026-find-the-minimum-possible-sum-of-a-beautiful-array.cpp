099b605e7621115ce8f15fcdd19ba583f73cb932
Harsh Srivastava <82330033+Harshstar@users.noreply.github.com>
2023-08-27 12:17:36 +0530

    Time: 202 ms (14.29%), Space: 47.3 MB (14.29%) - LeetHub

--- 3026-find-the-minimum-possible-sum-of-a-beautiful-array/3026-find-the-minimum-possible-sum-of-a-beautiful-array.cpp
+++ 3026-find-the-minimum-possible-sum-of-a-beautiful-array/3026-find-the-minimum-possible-sum-of-a-beautiful-array.cpp
@@ -5,25 +5,21 @@ public:
             return 0;
         long long sum=0;
         set<int>s;
-        s.insert(1);
-        for(int i=2;;i++)
+        for(int i=1;;i++)
         {
             if(s.size()==n)
                 break;
-            int temp=target-i;
-            if(s.find(temp)==s.end() && target>=i)
-            {
-                s.insert(i);
-                sum=sum+i;
-            }
-            if(s.size()==n)
-                break;
-            else if(i>target)
+            if(i>target)
             {
                 s.insert(i);
                 sum=sum+i;
             }  
+            else if(s.find(target-i)==s.end() && target>=i)
+            {
+                s.insert(i);
+                sum=sum+i;
+            }
         }
-        return sum+1;
+        return sum;
     }
 };
