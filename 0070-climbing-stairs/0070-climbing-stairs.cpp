83cc2038b47b0e6cb480b8759f6d711ffd75b453
Harsh Srivastava <82330033+Harshstar@users.noreply.github.com>
2023-09-02 16:21:59 +0530

    Time: 2 ms (43.58%), Space: 5.9 MB (76.4%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,11 @@
+vector<int>dp(46,-1);
 class Solution {
 public:
     int climbStairs(int n) {
-        int arr[n+1];
-        arr[0]=1;
-        arr[1]=1;
-        for(int i=2;i<=n;i++)
-        {
-            arr[i]= arr[i-1]+arr[i-2];
-        }
-        return arr[n];
+        if(dp[n]!=-1)
+            return dp[n];
+        if(n<=2)
+            return dp[n]=n;
+        return dp[n]=climbStairs(n-1)+climbStairs(n-2);
     }
 };
