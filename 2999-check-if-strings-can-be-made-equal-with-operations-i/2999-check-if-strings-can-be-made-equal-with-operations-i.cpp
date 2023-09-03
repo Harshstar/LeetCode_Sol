a159f9ab5534f77d41ffa51e1b185f94864a7591
Harsh Srivastava <82330033+Harshstar@users.noreply.github.com>
2023-09-03 06:15:59 +0530

    Time: 0 ms (100%), Space: 5.9 MB (60%) - LeetHub

---
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/2999-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+        string a="",b="",c="",d="";
+        for(int i=0;i<s1.size();i+=2)
+            a+=s1[i];
+        for(int i=1;i<s1.size();i+=2)
+            b+=s1[i];
+        for(int i=0;i<s2.size();i+=2)
+            c+=s2[i];
+        for(int i=1;i<s2.size();i+=2)
+            d+=s2[i];
+
+        sort(a.begin(),a.end());
+        sort(b.begin(),b.end());
+        sort(c.begin(),c.end());
+        sort(d.begin(),d.end());
+
+        return (a==c && b==d);
+    }
+};
