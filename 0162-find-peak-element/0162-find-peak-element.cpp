ac67b2d8308621e31bca76e6415888a2dc40f7cf
Harsh Srivastava <82330033+Harshstar@users.noreply.github.com>
2023-09-04 22:54:27 +0530

    Time: 3 ms (74.57%), Space: 9 MB (16.95%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -4,22 +4,20 @@ public:
         if(nums.size()<=1)
             return 0;
         if(nums[0]>nums[1])
-            return 0;
-        if(nums[0]<nums[1] && nums.size()==2)
-            return 1;
-        
+            return 0;   
         if(nums[nums.size()-1]>nums[nums.size()-2])
             return nums.size()-1;
         int j=nums.size()-2,i=1;
         while(i<=j)
         {
-            if(nums[i]>nums[i-1] && nums[i]>nums[i+1])
-                return i;
-            if(nums[j]>nums[j-1] && nums[j]>nums[j+1])
-                return j;
-            i++;
-            j--;
+            int mid=(i+j)/2;
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1])
+                return mid;
+            else if(nums[mid]>nums[mid-1])
+                i=mid+1;
+            else
+            j=mid-1;
         }
-        return i;
+        return -1;
     }
 };
