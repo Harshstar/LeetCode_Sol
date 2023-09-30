21f9faefab585cc6570d0b3ecb7043a26c7f42cb
Harsh Srivastava <82330033+Harshstar@users.noreply.github.com>
2023-09-30 21:41:07 +0530

    Time: 0 ms (100%), Space: 19.2 MB (16.67%) - LeetHub

---
+++ 3044-minimum-operations-to-collect-elements/3044-minimum-operations-to-collect-elements.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        set<int>s;
+        int c=0;
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+            c++;
+            if(nums[i]<=k)
+                s.insert(nums[i]);
+            if(s.size()==k)
+                break;
+        }
+        return c;
+    }
+};
