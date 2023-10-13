70025a1e04339e8c67396344e43aff2eaafd9211
Harsh Srivastava <82330033+Harshstar@users.noreply.github.com>
2023-10-13 23:09:44 +0530

    Time: 0 ms (100%), Space: 6.1 MB (45.03%) - LeetHub

--- 2663-distribute-money-to-maximum-children/2663-distribute-money-to-maximum-children.cpp
+++ 2663-distribute-money-to-maximum-children/2663-distribute-money-to-maximum-children.cpp
@@ -5,12 +5,12 @@ public:
         if(money<children) 
             return -1;
         int ans=0;        
-        for(int n=1; n<=children; n++)
+        for(int n=1;n<=children;n++)
         {
             int rem=money-n*8;
-            if(rem < children-n) 
+            if(rem<children-n) 
                 continue;
-            if(rem > 0 && (children-n)==0) 
+            if(rem>0 && (children-n)==0) 
                 continue; 
             if(rem==4 && (children-n)==1) 
                 continue;
