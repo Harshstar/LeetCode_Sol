8de348300336bd4b02c47157a0107abf3f229ec6
Harsh Srivastava <82330033+Harshstar@users.noreply.github.com>
2023-10-15 17:55:16 +0530

    Time: 26 ms (78.95%), Space: 7.7 MB (89.47%) - LeetHub

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/3150-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/3150-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
-        int n=s.size();
-        string ans;
-        int i=0,j=0;
-        int c=0;
-        while(j<n)
-        {
-            if(s[j]=='1')
-                c++;
-            if(c==k)
-            {   
-                while(i<n && c==k)
-                {
-                    string s1=s.substr(i,j-i+1);
-                    if(ans.size()==0 || s1.size()<ans.size())
-                        ans=s1;
-                    else if(s1.size()==ans.size())
-                        ans=min(ans,s1);
-                    if(s[i]=='1')
-                        c--;
-                    i++;
+        int n = s.size();
+        string ans = "";
+        
+        for(int i = 0; i < n; i++){
+            string curr = "";
+            for(int j = i; j<n; j++){
+                curr += s[j];
+                if(count(curr.begin(), curr.end(), '1') == k){
+                    if(ans == "")
+                        ans = curr;
+                    else if(ans.size() > curr.size())
+                        ans = curr;
+                    else if(ans.size() == curr.size())
+                        ans = min(ans, curr);
                 }
             }
-            j++;
+            
         }
+        
         return ans;
     }
 };
