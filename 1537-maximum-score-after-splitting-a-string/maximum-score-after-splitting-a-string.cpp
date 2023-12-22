cf60293d49da6487275cf1585d7c18bd417a6b1d
Harsh Srivastava <82330033+Harshstar@users.noreply.github.com>
2023-12-22 15:50:08 +0530

    Time: 0 ms (100.00%) | Memory: 6.8 MB (43.75%) - LeetSync

---
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxScore(string s) {
+        if(s.size()==2 && s[0]==s[1])
+            return 1;
+        unordered_map<char,int>mp1;
+        for(auto x:s)
+            mp1[x]++;
+        unordered_map<char,int>mp2;
+        int maxi=INT_MIN;   
+        for(int i=0;i<s.size()-1;i++)
+        {
+            mp2[s[i]]++;
+            mp1[s[i]]--;
+            int x=mp2['0'];
+            int y=mp1['1'];
+            maxi=max(maxi,(x+y));
+        }
+        return maxi;
+    }
+};
